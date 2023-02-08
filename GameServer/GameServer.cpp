1d213f262d05e89e53540acce9582588dc921352
현광빈 <bio8641@naver.com>
2023-02-08 17:31:58 +0900

    TypeCast + Socket

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,47 +1,101 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-#include <thread>
 #include <atomic>
 #include <mutex>
 #include <Windows.h>
 #include <future>
 #include "ThreadManager.h"
 
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
-
-class Knight
-{
-public:
-	int32 _hp = rand() % 1000;
-};
-
-class Monster
-{
-public:
-	int64 _id = 0;
-};
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	for (int32 i = 0; i < 5; i++)
-	{
-		GThreadManager->Launch([]()
-			{
-				while (true)
-				{
-					Knight* knight = xnew<Knight>();
+    WSAData wsaData;
+    if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+        return 0;
 
-					cout << knight->_hp << endl;
+    SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket == INVALID_SOCKET)
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Socket ErrorCode : " << errCode << endl;
+        return 0;
+    }
 
-					this_thread::sleep_for(10ms);
+    SOCKADDR_IN serverAddr; // IPv4
+    ::memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 너가 알아서 해줘
+    serverAddr.sin_port = ::htons(7777); // 80 : HTTP
 
-					xdelete(knight);
-				}
-			});
-	}
+    if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Bind ErrorCode : " << errCode << endl;
+        return 0;
+    }
 
-	GThreadManager->Join();
+    // 영업 시작!
+    if (::listen(listenSocket, 10) == SOCKET_ERROR)
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Listen ErrorCode : " << errCode << endl;
+        return 0;
+    }
+
+    // --------------------------------
+
+    while (true)
+    {
+        SOCKADDR_IN clientAddr; // IPv4
+        ::memset(&clientAddr, 0, sizeof(clientAddr));
+        int32 addrLen = sizeof(clientAddr);
+
+        SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+        if (clientSocket == INVALID_SOCKET)
+        {
+            int32 errCode = ::WSAGetLastError();
+            cout << "Accept ErrorCode : " << errCode << endl;
+            return 0;
+        }
+
+        // 손님 입장!
+        char ipAdress[16];
+        ::inet_ntop(AF_INET, &clientAddr, ipAdress, sizeof(ipAdress));
+        cout << "Client Connected! IP = " << ipAdress << endl;
+
+        // TODO
+        while (true)
+        {
+            char recvBuffer[1000];
+
+            int recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+            if (recvLen <= 0)
+            {
+                int32 errCode = ::WSAGetLastError();
+                cout << "Recv ErrorCode : " << errCode << endl;
+                return 0;
+            }
+
+            cout << "Recv Data! Data = " << recvBuffer << endl;
+            cout << "Recv Data! Len = " << recvLen << endl;
+
+            int32 resultCode = ::send(clientSocket, recvBuffer, recvLen, 0);
+            if (resultCode == SOCKET_ERROR)
+            {
+                int32 errCode = ::WSAGetLastError();
+                cout << "Send ErrorCode : " << errCode << endl;
+                return 0;
+            }
+        }
+    }
+
+    // --------------------------------
+
+    // 윈속 종료
+    ::WSACleanup();
 }
