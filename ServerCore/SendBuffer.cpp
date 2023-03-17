cc586595f24569e3eee8612983545633bc8646a7
BEEPMan <34468922+BEEPMan@users.noreply.github.com>
2023-03-17 23:44:25 +0900

    Chatting Example

--- ServerCore/SendBuffer.cpp
+++ ServerCore/SendBuffer.cpp
@@ -79,15 +79,11 @@ SendBufferRef SendBufferManager::Open(uint32 size)
 		LSendBufferChunk->Reset();
 	}
 
-	cout << "FREE : " << LSendBufferChunk->FreeSize() << endl;
-
 	return LSendBufferChunk->Open(size);
 }
 
 SendBufferChunkRef SendBufferManager::Pop()
 {
-	cout << "Pop SENDBUFFERCHUNK" << endl;
-
 	{
 		WRITE_LOCK;
 		if (_sendBufferChunks.empty() == false)
@@ -109,7 +105,5 @@ void SendBufferManager::Push(SendBufferChunkRef buffer)
 
 void SendBufferManager::PushGlobal(SendBufferChunk* buffer)
 {
-	cout << "PushGlobal SENDBUFFERCHUNK" << endl;
-
 	GSendBufferManager->Push(SendBufferChunkRef(buffer, PushGlobal));
 }
